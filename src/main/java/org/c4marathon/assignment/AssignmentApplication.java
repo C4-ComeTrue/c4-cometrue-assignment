9a2af6fef28a053e69024943973ebfcddc6c0275
VSFe <klm03025@gmail.com>
2023-12-27 23:09:42 +0900

    chore: setting project

---
+++ src/main/java/org/c4marathon/assignment/AssignmentApplication.java
@@ -0,0 +1,13 @@
+package org.c4marathon.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentApplication.class, args);
+	}
+
+}
