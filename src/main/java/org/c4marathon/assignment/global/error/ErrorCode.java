edfdb2d1ac9eedf449b04a1c8150821e2aa3a67d
kariskan <zkfzpf56@naver.com>
2023-12-31 17:01:29 +0900

    feat: 예외 처리를 위한 ErrorCode 작성

---
+++ src/main/java/org/c4marathon/assignment/global/error/ErrorCode.java
@@ -0,0 +1,23 @@
+package org.c4marathon.assignment.global.error;
+
+import static org.springframework.http.HttpStatus.*;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+
+	CONSUMER_NEED_ADDRESS(BAD_REQUEST, "소비자인 경우 회원가입 시 주소가 필요합니다."),
+	ALREADY_CONSUMER_EXISTS(CONFLICT, "이미 이메일에 해당하는 소비자가 존재합니다."),
+	;
+
+	private final HttpStatus status;
+	private final String message;
+
+	ErrorCode(HttpStatus httpStatus, String message) {
+		this.status = httpStatus;
+		this.message = message;
+	}
+}
