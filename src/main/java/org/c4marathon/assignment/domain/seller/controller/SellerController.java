bdce873f06f3dc98a37b9977e0a978257ce8f084
kariskan <zkfzpf56@naver.com>
2024-01-04 19:18:30 +0900

    refactor: ResponseStatus 추가

--- src/main/java/org/c4marathon/assignment/domain/seller/controller/SellerController.java
+++ src/main/java/org/c4marathon/assignment/domain/seller/controller/SellerController.java
@@ -4,9 +4,11 @@ import org.c4marathon.assignment.domain.seller.dto.request.PutProductRequest;
 import org.c4marathon.assignment.domain.seller.service.SellerService;
 import org.c4marathon.assignment.global.auth.SellerThreadLocal;
 import org.c4marathon.assignment.global.response.ResponseDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
@@ -19,6 +21,7 @@ public class SellerController {
 
 	private final SellerService sellerService;
 
+	@ResponseStatus(HttpStatus.CREATED)
 	@PostMapping("/products")
 	public ResponseDto<Void> putProduct(@RequestBody @Valid PutProductRequest request) {
 		sellerService.putProduct(request, SellerThreadLocal.get());
