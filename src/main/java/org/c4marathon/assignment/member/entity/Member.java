8c4f47bd50ee7146a5ef40ecd648304709eb74da
youngsoosoo <lovepys01@naver.com>
2024-01-04 23:43:33 +0900

    feat: Member Entity 작성

---
+++ src/main/java/org/c4marathon/assignment/member/entity/Member.java
@@ -0,0 +1,45 @@
+package org.c4marathon.assignment.member.entity;
+
+import org.c4marathon.assignment.util.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Entity
+@Table(name = "member")
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true, updatable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Builder
+    public Member(String email, String password, String name) {
+
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+}
