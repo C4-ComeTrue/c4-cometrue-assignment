21bafef0024524878c089a518f6189941f7a1ba1
youngsoosoo <lovepys01@naver.com>
2024-01-05 18:26:12 +0900

    feat: MemberRepository 추가

---
+++ src/main/java/org/c4marathon/assignment/member/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package org.c4marathon.assignment.member.repository;
+
+import org.c4marathon.assignment.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
