08f56a466009a2924c0f0662fbd2edfbd7302566
youngsoosoo <lovepys01@naver.com>
2024-01-05 18:34:39 +0900

    feat: 회원가입과 로그인을 위한 메서드 선언

--- src/main/java/org/c4marathon/assignment/member/repository/MemberRepository.java
+++ src/main/java/org/c4marathon/assignment/member/repository/MemberRepository.java
@@ -1,9 +1,15 @@
 package org.c4marathon.assignment.member.repository;
 
+import java.util.Optional;
+
 import org.c4marathon.assignment.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<Member> findByEmail(String email);
 }
