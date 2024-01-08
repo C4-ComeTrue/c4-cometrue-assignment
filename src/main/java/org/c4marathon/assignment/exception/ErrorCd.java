0f80919230e136dad50c982f18fa9e78a1d5e3dc
Yunsik Kim <yunsik0115@uos.ac.kr>
2024-01-08 16:45:19 +0900

    feat: 예외처리 클래스 생성

---
+++ src/main/java/org/c4marathon/assignment/exception/ErrorCd.java
@@ -0,0 +1,27 @@
+package org.c4marathon.assignment.exception;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum ErrorCd {
+	INVALID_ARGUMENT(HttpStatus.BAD_REQUEST, "Invalid Argument"),
+	NOT_FOUND(HttpStatus.NOT_FOUND, "HTTP not found"),
+	NO_PERMISSION(HttpStatus.FORBIDDEN, "No Permission"),
+	NOT_EXIST_USER(HttpStatus.NOT_FOUND, "User Not Found"),
+	NO_SUCH_ITEM(HttpStatus.NOT_FOUND, "Item Not Found");
+
+	private final HttpStatus httpStatus;
+	private final String message;
+
+	public ServiceException serviceException() {
+		return new ServiceException(this.name(), message);
+	}
+
+	public ServiceException serviceException(String debugMessage, Object...debugMessageArgs) {
+		return new ServiceException(this.name(), message, String.format(debugMessage, debugMessageArgs));
+	}
+}
---
+++ src/main/java/org/c4marathon/assignment/exception/ServiceException.java
@@ -0,0 +1,54 @@
+package org.c4marathon.assignment.exception;
+
+import io.micrometer.common.util.StringUtils;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+public class ServiceException extends RuntimeException {
+	private final String errCode;
+	private final String errMessage;
+	private final String debugMessage; // 로깅에 굉장히 중요해짐.
+
+	public ServiceException(String errCode, String errMessage, String debugMessage) {
+		super(getExceptionMessage(errCode, errMessage, debugMessage));
+		this.errCode = errCode;
+		this.errMessage = errMessage;
+		this.debugMessage = debugMessage;
+	}
+
+	public ServiceException(String errCode, String errMessage, String debugMessage, Throwable cause) {
+		super(getExceptionMessage(errCode, errMessage, debugMessage), cause);
+		this.errCode = errCode;
+		this.errMessage = errMessage;
+		this.debugMessage = debugMessage;
+	}
+
+	public ServiceException(String errCode, String errMessage) {
+		super(getExceptionMessage(errCode, errMessage, null));
+		this.errCode = errCode;
+		this.errMessage = errMessage;
+		this.debugMessage = null;
+	}
+
+	public ServiceException(String errCode, String errMessage, Throwable cause) {
+		super(getExceptionMessage(errCode, errMessage, null), cause);
+		this.errCode = errCode;
+		this.errMessage = errMessage;
+		this.debugMessage = null;
+	}
+
+	private static String getExceptionMessage(String errCode, String errMessage, String debugMessage) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(errCode);
+		sb.append(" : ");
+		sb.append(errMessage);
+
+		if (!StringUtils.isEmpty(debugMessage)) {
+			sb.append(" - ");
+			sb.append(debugMessage);
+		}
+
+		return sb.toString();
+	}
+}
