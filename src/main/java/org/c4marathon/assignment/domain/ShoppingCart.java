57fdd1f6dc2c5ea96961028ad8d95c5e67e40f85
Yunsik Kim <yunsik0115@uos.ac.kr>
2024-01-09 21:04:34 +0900

    feat: 상품을 담아두기 위한 상품카트 도메인 객체 추가

---
+++ src/main/java/org/c4marathon/assignment/domain/ShoppingCart.java
@@ -0,0 +1,38 @@
+package org.c4marathon.assignment.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class ShoppingCart {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long shoppingCartId;
+
+	@OneToMany
+	private List<Item> itemList;
+
+	@OneToOne
+	private Member member;
+
+	public ShoppingCart(Member member) {
+		this.itemList = new ArrayList<>();
+		this.member = member;
+	}
+
+	public void addItemToCart(Item item) {
+		this.itemList.add(item);
+	}
+}
