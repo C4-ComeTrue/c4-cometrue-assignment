7e39f4bda76b9640442434ff0a2362408bceb23d
Yunsik Kim <yunsik0115@uos.ac.kr>
2024-01-10 13:59:49 +0900

    fix: 주문 비즈니스 로직 변경으로 인한 Order, OrderItem 엔티티 수정

--- src/main/java/org/c4marathon/assignment/domain/Order.java
+++ src/main/java/org/c4marathon/assignment/domain/Order.java
@@ -1,7 +1,12 @@
 package org.c4marathon.assignment.domain;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.hibernate.annotations.ColumnDefault;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -9,9 +14,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Table(name = "ORD")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Order {
 
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,6 +34,9 @@ public class Order {
 	@ManyToOne
 	private Member customer;
 
+	@OneToMany
+	private List<OrderItem> orderItems = new ArrayList<>();
+
 	@OneToOne
 	private Payment payment;
 
@@ -27,8 +44,5 @@ public class Order {
 
 	@OneToOne
 	private Shipment shipment;
-
-	@Enumerated(EnumType.STRING)
-	private OrderStatus orderStatus;
 
 }
--- src/main/java/org/c4marathon/assignment/domain/OrderItem.java
+++ src/main/java/org/c4marathon/assignment/domain/OrderItem.java
@@ -3,13 +3,21 @@ package org.c4marathon.assignment.domain;
 import org.hibernate.annotations.ColumnDefault;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class OrderItem {
 
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,5 +39,9 @@ public class OrderItem {
 	@ColumnDefault("false")
 	@Column(columnDefinition = "TINYINT(1)")
 	private boolean isProceeded; // 이 주문 내역의 승인여부를 표시합니다.
+
+	public int getTotalPrice(){
+		return this.price * this.count;
+	}
 
 }
