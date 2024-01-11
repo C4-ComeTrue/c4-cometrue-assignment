05e6c49a1d5d8618d9c47b83e9e237fef779b459
Yunsik Kim <yunsik0115@uos.ac.kr>
2024-01-11 20:42:19 +0900

    feat: 반품 기록을 위한 Refund 엔티티 생성

---
+++ src/main/java/org/c4marathon/assignment/domain/Refund.java
@@ -0,0 +1,39 @@
+package org.c4marathon.assignment.domain;
+
+import java.time.LocalDateTime;
+
+import org.springframework.cglib.core.Local;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Refund {
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long refundId;
+
+	@OneToOne
+	private Order order;
+
+	@OneToOne
+	private Shipment shipment;
+
+	@OneToOne
+	private Member customer;
+
+	@OneToOne
+	private Member seller;
+
+	@Enumerated(EnumType.STRING)
+	private RefundStatus refundStatus;
+
+	private LocalDateTime refundRequestedDate;
+
+	private LocalDateTime refundCompletedDate;
+}
---
+++ src/main/java/org/c4marathon/assignment/domain/RefundStatus.java
@@ -0,0 +1,7 @@
+package org.c4marathon.assignment.domain;
+
+public enum RefundStatus {
+	PENDING,
+	AWAITING_SHIPMENT,
+	COMPLETED
+}
