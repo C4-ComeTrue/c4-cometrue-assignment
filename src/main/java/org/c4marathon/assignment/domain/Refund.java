a952e624f7815dcb88f278b4ab336b5e4d1b6e3e
Yunsik Kim <yunsik0115@uos.ac.kr>
2024-01-14 13:08:36 +0900

    feat: Refund 엔티티 getter, setter 추가

--- src/main/java/org/c4marathon/assignment/domain/Refund.java
+++ src/main/java/org/c4marathon/assignment/domain/Refund.java
@@ -11,8 +11,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Refund {
 
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
