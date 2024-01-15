5c26f7ce04274b8ce820bfc44bc73749e32d8aaf
Yunsik Kim <yunsik0115@uos.ac.kr>
2024-01-15 10:33:39 +0900

    feat: PaymentService 금액 입금, 금액 출금, 판매자 잔고 입금, 반품에 의한 출금

---
+++ src/main/java/org/c4marathon/assignment/service/PaymentService.java
@@ -0,0 +1,94 @@
+package org.c4marathon.assignment.service;
+
+import org.c4marathon.assignment.domain.Member;
+import org.c4marathon.assignment.domain.MemberType;
+import org.c4marathon.assignment.domain.Payment;
+import org.c4marathon.assignment.domain.ChargeType;
+import org.c4marathon.assignment.exception.ErrorCd;
+import org.c4marathon.assignment.repository.PaymentRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class PaymentService {
+
+	private final PaymentRepository paymentRepository;
+	private final MemberService memberService;
+
+	public Payment charge(int value, Member member) {
+
+		if (value <= 0){
+			throw ErrorCd.INVALID_ARGUMENT.serviceException("충전 금액을 확인하세요",
+				"잘못된 충전 금액 (입력값) : ", value);
+		}
+
+		Payment payment = new Payment(
+			member,
+			ChargeType.CHARGE,
+			value
+		);
+
+		paymentRepository.save(payment);
+		return payment;
+	}
+
+	public Payment discharge(int value, Member member){
+
+		if (value <= 0) {
+			throw ErrorCd.INTERNAL_SERVER_ERROR.serviceException("결제 시도 금액 이상", value);
+		}
+
+		if (getBalance(member.getMemberPk()) < value) {
+			throw ErrorCd.INVALID_ARGUMENT.serviceException("결제 이상", "한도 초과");
+		}
+
+		Payment payment = new Payment(
+			member,
+			ChargeType.DISCHARGE,
+			value
+		);
+
+		return paymentRepository.save(payment);
+	}
+
+	public void sellerCharge(int value, Member member) {
+		if ( member.getMemberType() != MemberType.ROLE_SELLER ) {
+			throw ErrorCd.NO_PERMISSION.serviceException("판매자만 수익을 올릴 수 있습니다.");
+		}
+
+		Payment payment = new Payment(
+			member,
+			ChargeType.COMMISSION,
+			value
+		);
+
+		paymentRepository.save(payment);
+	}
+
+	// 요구 조건 상, 구매 확인 이전까지만 환불이 가능함.
+	// 따라서 구매 확정 전에는 seller 잔고는 변화할 일이 없음.
+	// 그러나, 통상적인 경우 판매 확인 이후에도 반품이 가능하기 때문에 이 메서드를 유지함.
+	public Payment sellerDischarge(int value, Member member) {
+		if ( member.getMemberType() != MemberType.ROLE_SELLER ) {
+			throw ErrorCd.NO_PERMISSION.serviceException("판매자만 반품 지출을 생성할 수 있습니다.");
+		}
+		Payment payment = new Payment(
+			member,
+			ChargeType.REFUND,
+			value
+		);
+
+		return paymentRepository.save(payment);
+
+	}
+
+	private int getBalance(Long memberId) {
+		Member member = memberService.findCustomerId(memberId);
+		return paymentRepository.currentBalance(member);
+	}
+
+}
