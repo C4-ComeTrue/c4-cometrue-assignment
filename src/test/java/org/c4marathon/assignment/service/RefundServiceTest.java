c893ead3e26ce9443f4cf603ebbf09c8137c67d9
Yunsik Kim <yunsik0115@uos.ac.kr>
2024-01-19 20:33:05 +0900

    test: RefundServiceTest 작성 - refund (PK 오류 있음)

---
+++ src/test/java/org/c4marathon/assignment/service/RefundServiceTest.java
@@ -0,0 +1,132 @@
+package org.c4marathon.assignment.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.c4marathon.assignment.domain.CartItem;
+import org.c4marathon.assignment.domain.ChargeType;
+import org.c4marathon.assignment.domain.Item;
+import org.c4marathon.assignment.domain.Member;
+import org.c4marathon.assignment.domain.MemberType;
+import org.c4marathon.assignment.domain.Order;
+import org.c4marathon.assignment.domain.OrderStatus;
+import org.c4marathon.assignment.domain.Payment;
+import org.c4marathon.assignment.domain.Refund;
+import org.c4marathon.assignment.service.dto.CartItemDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefundServiceTest {
+
+	private static Long sellerId;
+	private static Long customerId;
+
+	private static Long itemId;
+
+	@Autowired
+	private MemberService memberService;
+
+	@Autowired
+	private CartItemService cartItemService;
+
+	@Autowired
+	private OrderService orderService;
+
+	@Autowired
+	private ItemService itemService;
+
+	@Autowired
+	private PaymentService paymentService;
+
+	@Autowired
+	private RefundService refundService;
+
+	@BeforeEach
+	void beforeEach(){
+		Member seller1 = new Member();
+		seller1.setUserId("noogler02");
+		seller1.setPostalCode("129-03");
+		seller1.setValid(true);
+		seller1.setPassword("test2");
+		seller1.setAddress("경기도 남양주시 경춘로");
+		seller1.setPhone("010-4822-2020");
+		seller1.setUsername("홍길동");
+		Member seller = memberService.register(seller1, MemberType.ROLE_SELLER);
+		sellerId = seller.getMemberPk();
+
+		Member customer1 = new Member();
+		customer1.setUserId("noogler");
+		customer1.setPostalCode("129-03");
+		customer1.setValid(true);
+		customer1.setPassword("test2");
+		customer1.setAddress("경기도 남양주시 경춘로");
+		customer1.setPhone("010-4822-2020");
+		customer1.setUsername("홍길동");
+		Member customer = memberService.register(customer1, MemberType.ROLE_CUSTOMER);
+		customerId = customer.getMemberPk();
+
+		paymentService.charge(20000000, customer);
+
+		Item item = new Item();
+		item.setName("상품 테스트");
+		item.setDescription("테스트 상품 설명");
+		item.setStock(200);
+		item.setPrice(20000);
+		item.setSeller(seller);
+		Item savedItem = itemService.saveItem(item, sellerId);
+		itemId = savedItem.getItemPk();
+
+		CartItemDTO cartItemDTO = new CartItemDTO();
+		cartItemDTO.setItemId(savedItem.getItemPk());
+		cartItemDTO.setCount(5); // 장바구니에 5개 담기
+
+		cartItemService.addCart(cartItemDTO, customer);
+
+	}
+
+
+	@Test
+	void refund() {
+		Member customer = memberService.findCustomerById(customerId);
+		List<CartItem> allCartItem = cartItemService.getAllCartItem(customer);
+		Item previousItem = itemService.findById(itemId);
+		Integer previousPurchaseStock = previousItem.getStock();
+		Order proceeded = orderService.proceed(allCartItem, customerId, sellerId);
+		Item updatedItem = itemService.findById(itemId);
+
+		Refund refund = refundService.refund(customerId, proceeded.getOrderPk());
+
+		Item updatedRefundedItem = itemService.findById(itemId);
+		Integer refundedPurchaseStock = updatedRefundedItem.getStock();
+
+		assertEquals(refundedPurchaseStock, previousPurchaseStock);
+		Payment payment = refund.getPayment();
+		assertEquals(payment.getValue(), updatedItem.getPrice() * 5);
+		assertEquals(payment.getValueType(), ChargeType.REFUND);
+	}
+
+	@Test
+	void refundOrderStatusException() {
+		Member customer = memberService.findCustomerById(customerId);
+		List<CartItem> allCartItem = cartItemService.getAllCartItem(customer);
+		Item previousItem = itemService.findById(itemId);
+		itemService.updateItem(previousItem, previousItem.getItemPk(), sellerId);
+		Integer previousPurchaseStock = previousItem.getStock();
+		Order proceeded = orderService.proceed(allCartItem, customerId, sellerId);
+		proceeded.setOrderStatus(OrderStatus.ORDERED_SHIPPED);
+
+		assertThrows(RuntimeException.class, () -> refundService.refund(customerId, proceeded.getOrderPk()));
+	}
+
+	@Test
+	void findById() {
+	}
+
+	@Test
+	void findBySeller() {
+	}
+}
