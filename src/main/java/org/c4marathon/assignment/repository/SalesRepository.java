38ddc9333ce438896fc3bf24ec38898fd64acc9b
Yunsik Kim <yunsik0115@uos.ac.kr>
2024-01-20 21:08:19 +0900

    refactor: seller 조회 where절 from, to 모두 확인하도록 변경

--- src/main/java/org/c4marathon/assignment/repository/SalesRepository.java
+++ src/main/java/org/c4marathon/assignment/repository/SalesRepository.java
@@ -4,13 +4,17 @@ import java.util.List;
 
 import org.c4marathon.assignment.domain.Item;
 import org.c4marathon.assignment.domain.Member;
+import org.c4marathon.assignment.domain.OrderItem;
 import org.c4marathon.assignment.domain.Sales;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface SalesRepository extends JpaRepository<Sales, Long> {
 
-	@Query("select s from Sales s where s.receiver = :seller")
+	@Query("select s from Sales s where s.orderItem = :orderItem")
+	List<Sales> findAllByOrderItem(OrderItem orderItem);
+
+	@Query("select s from Sales s where s.receiver = :seller or s.sender = :seller")
 	List<Sales> findAllBySeller(Member seller);
 
 	@Query("select s from Sales s join fetch OrderItem o where o.item = :item")
