7556fb6c0320a419ed387a900962d4abda5307b8
youngsoosoo <lovepys01@naver.com>
2024-01-29 01:39:13 +0900

    test: testImplementaion 의존성 등록, h2 DB에 맞도록 수정

--- src/test/resources/sql/CreateData.sql
+++ src/test/resources/sql/CreateData.sql
@@ -1,14 +1,11 @@
 -- 회원 데이터 삽입
 INSERT INTO member (email, password, name, status, create_dt, update_dt)
-VALUES ('test@naver.com', 'test', 'Test User', 'ACTIVE', NOW(), NOW());
-
--- 방금 생성된 회원의 ID를 가져옵니다.
-SET @member_id = LAST_INSERT_ID();
+VALUES ('test@naver.com', 'test', 'Test User', 'ACTIVE', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
 -- 메인 계좌 데이터 삽입
 INSERT INTO account (balance, daily_limit, type, member_id, status, create_dt, update_dt)
-VALUES (10000, 10000, 'REGULAR_ACCOUNT', @member_id, 'ACTIVE', NOW(), NOW());
+VALUES (10000, 10000, 'REGULAR_ACCOUNT', 1L, 'ACTIVE', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
 -- 적금 계좌 데이터 삽입
 INSERT INTO account (balance, daily_limit, type, member_id, status, create_dt, update_dt)
-VALUES (0, 0, 'INSTALLMENT_SAVINGS_ACCOUNT', @member_id, 'ACTIVE', NOW(), NOW());
+VALUES (0, 0, 'INSTALLMENT_SAVINGS_ACCOUNT', 1L, 'ACTIVE', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
