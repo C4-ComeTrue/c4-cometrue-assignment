dd25237f4695808928744241da5f24feab51d063
seungh1024 <seungh1024@naver.com>
2024-02-02 01:01:36 +0900

    refactor: 메인 테스트 클래스에 @ContextConfiguration 추가

--- src/test/java/org/c4marathon/assignment/AssignmentApplicationTests.java
+++ src/test/java/org/c4marathon/assignment/AssignmentApplicationTests.java
@@ -2,7 +2,9 @@ package org.c4marathon.assignment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
+@ContextConfiguration(classes = AssignmentApplication.class)
 @SpringBootTest
 class AssignmentApplicationTests {
 
