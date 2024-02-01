634618e73356aaf4b58acf09f0384c00b0a04304
seungh1024 <seungh1024@naver.com>
2024-02-02 01:29:50 +0900

    refactor: EnvConfig 삭제

--- src/main/java/org/c4marathon/assignment/common/config/EnvConfig.java
+++
@@ -1,9 +0,0 @@
-package org.c4marathon.assignment.common.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("classpath:properties/env.properties")
-public class EnvConfig {
-}
--- src/test/java/org/c4marathon/assignment/AssignmentApplicationTests.java
+++ src/test/java/org/c4marathon/assignment/AssignmentApplicationTests.java
@@ -2,9 +2,7 @@ package org.c4marathon.assignment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 
-@ContextConfiguration(classes = AssignmentApplication.class)
 @SpringBootTest
 class AssignmentApplicationTests {
 
