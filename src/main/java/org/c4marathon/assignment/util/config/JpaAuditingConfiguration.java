fc960a13b4d51cb3de983f6374f99c874f277b72
youngsoosoo <lovepys01@naver.com>
2024-02-08 16:19:54 +0900

    refactor: JpaAuditingConfiguration 설정 분리

---
+++ src/main/java/org/c4marathon/assignment/util/config/JpaAuditingConfiguration.java
@@ -0,0 +1,9 @@
+package org.c4marathon.assignment.util.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+}
