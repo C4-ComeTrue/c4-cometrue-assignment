64959f4e88b9f066c306c323493332a6a09fce4e
MilanoBeer <tgd5753@gmail.com>
2024-02-13 23:43:14 +0900

    feat : Todo Validator - target type에 따라 구분하여 날짜 선후관계 검증

--- src/main/java/org/c4marathon/assignment/annotation/TodoValidator.java
+++ src/main/java/org/c4marathon/assignment/annotation/TodoValidator.java
@@ -1,6 +1,9 @@
 package org.c4marathon.assignment.annotation;
 
+import java.util.Date;
+
 import org.c4marathon.assignment.todo.dto.CreateTodoRequest;
+import org.c4marathon.assignment.todo.dto.EditTodoRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -10,16 +13,34 @@ public class TodoValidator implements Validator {
 
 	@Override
 	public boolean supports(Class<?> clazz) {
-		return CreateTodoRequest.class.isAssignableFrom(clazz); // *** ERROR ***
+		return clazz.isAssignableFrom(clazz);
 	}
 
+	/**
+	 * target의 request타입을 구별하여 날짜순서에 대한 유효성 검사 진행
+	 * @param target 유효성 검사 대상
+	 * @param errors 유효성 검사 상태정보
+	 */
 	@Override
 	public void validate(Object target, Errors errors) {
-		// 내부에서 형변환
-		CreateTodoRequest dto = (CreateTodoRequest) target;
+		if (target instanceof CreateTodoRequest) {
+			CreateTodoRequest dto = (CreateTodoRequest) target;
+			checkDate(dto.startDay(), dto.endDay(), errors);
+		}
+		else if (target instanceof EditTodoRequest) {
+			EditTodoRequest dto = (EditTodoRequest) target;
+			checkDate(dto.startDay(), dto.endDay(), errors);
+		}
+	}
 
-		// 검증
-		if(dto.startDay().after(dto.endDay())) {
+	/**
+	 * 날짜의 선후관계 검증
+	 * @param startDay
+	 * @param endDay
+	 * @param errors
+	 */
+	private void checkDate(Date startDay, Date endDay, Errors errors) {
+		if (startDay.after(endDay)) {
 			errors.rejectValue("startDay", "notEmail", "email 형식으로 입력해주세요.");
 		}
 	}
