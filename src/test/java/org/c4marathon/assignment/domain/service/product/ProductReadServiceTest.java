edb548b37bc4355340c718752e1d9e29675af213
kariskan <zkfzpf56@naver.com>
2024-05-22 01:31:11 +0900

    test: @SpringBootTest 제거

--- src/test/java/org/c4marathon/assignment/domain/service/product/ProductReadServiceTest.java
+++ src/test/java/org/c4marathon/assignment/domain/service/product/ProductReadServiceTest.java
@@ -23,7 +23,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.mockito.InjectMocks;
-import org.springframework.boot.test.context.SpringBootTest;
 
 public class ProductReadServiceTest extends ServiceTestSupport {
 
