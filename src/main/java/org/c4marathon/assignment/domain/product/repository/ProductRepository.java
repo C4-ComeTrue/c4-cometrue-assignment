fcc83f15e0da72b7b8a9ed45d8a8f5d353ba3b50
kariskan <zkfzpf56@naver.com>
2024-05-21 21:16:45 +0900

    feat: 검색 조건에 따른 쿼리 작성
    - 최신순
    - 가격 높은 순
    - 가격 낮은 순
    - 많이 구매한 순
    - 리뷰 평점 높은 순

--- src/main/java/org/c4marathon/assignment/domain/product/repository/ProductRepository.java
+++ src/main/java/org/c4marathon/assignment/domain/product/repository/ProductRepository.java
@@ -1,5 +1,7 @@
 package org.c4marathon.assignment.domain.product.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import org.c4marathon.assignment.domain.product.entity.Product;
@@ -18,4 +20,88 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	@Query("select p from Product p join fetch p.seller where p.id = :id")
 	Optional<Product> findByIdJoinFetch(@Param("id") Long id);
+
+	@Query(value = """
+		select count(*)
+		from product_tbl p
+		where p.product_id = :product_id
+		""", nativeQuery = true)
+	Long findReviewCount(@Param("product_id") Long id);
+
+	@Query(value = """
+		select *
+		from product_tbl p
+		where name like :keyword
+		  and (p.created_at < :createdAt or (p.created_at = :createdAt and p.product_id > :id))
+		order by p.created_at desc, p.product_id
+		limit :pageSize
+		""", nativeQuery = true)
+	List<Product> findByNewest(
+		@Param("keyword") String keyword,
+		@Param("createdAt") LocalDateTime createdAt,
+		@Param("id") Long id,
+		@Param("pageSize") int pageSize
+	);
+
+	@Query(value = """
+		select *
+		from product_tbl p
+		where name like :keyword
+		  and (p.amount > :amount or (p.amount = :amount and p.product_id > :id))
+		order by p.amount, p.product_id
+		limit :pageSize
+		""", nativeQuery = true)
+	List<Product> findByPriceAsc(
+		@Param("keyword") String keyword,
+		@Param("amount") Long amount,
+		@Param("id") Long id,
+		@Param("pageSize") int pageSize
+	);
+
+	@Query(value = """
+		select *
+		from product_tbl p
+		where (p.amount < :amount or (p.amount = :amount and p.product_id > :id))
+		  and name like :keyword
+		order by p.amount desc, p.product_id
+		limit :pageSize
+		""", nativeQuery = true)
+	List<Product> findByPriceDesc(
+		@Param("keyword") String keyword,
+		@Param("amount") Long amount,
+		@Param("id") Long id,
+		@Param("pageSize") int pageSize
+	);
+
+	@Query(value = """
+		select *
+		from product_tbl
+		where name like :keyword
+		    and order_count <= :order_count
+		   or (order_count = :order_count and product_id > :product_id)
+		order by order_count desc, product_id
+		limit :pageSize
+		""", nativeQuery = true)
+	List<Product> findByPopularity(
+		@Param("keyword") String keyword,
+		@Param("order_count") Long orderCount,
+		@Param("product_id") Long id,
+		@Param("pageSize") int pageSize
+	);
+
+	@Query(value = """
+		select *
+		from product_tbl
+		where name like :keyword
+		    and avg_score <= :avgScore
+		   or (avg_score = :avgScore and product_id > :product_id)
+		order by avg_score desc, product_id
+		limit :pageSize
+		""", nativeQuery = true)
+	List<Product> findByTopRated(
+		@Param("keyword") String keyword,
+		@Param("avgScore") Double avgScore,
+		@Param("product_id") Long id,
+		@Param("pageSize") int pageSize
+	);
 }
--- src/main/java/org/c4marathon/assignment/domain/product/service/ProductReadService.java
+++ src/main/java/org/c4marathon/assignment/domain/product/service/ProductReadService.java
@@ -1,5 +1,9 @@
 package org.c4marathon.assignment.domain.product.service;
 
+import java.util.List;
+
+import org.c4marathon.assignment.domain.product.dto.request.ProductSearchRequest;
+import org.c4marathon.assignment.domain.product.dto.response.ProductSearchResponse;
 import org.c4marathon.assignment.domain.product.entity.Product;
 import org.c4marathon.assignment.domain.product.repository.ProductRepository;
 import org.c4marathon.assignment.domain.seller.entity.Seller;
@@ -30,5 +34,39 @@ public class ProductReadService {
 	public Product findById(Long id) {
 		return productRepository.findByIdJoinFetch(id)
 			.orElseThrow(() -> ErrorCode.PRODUCT_NOT_FOUND.baseException("id: %d", id));
+	}
+
+	@Transactional(readOnly = true)
+	public Long findReviewCount(Long productId) {
+		return productRepository.findReviewCount(productId);
+	}
+
+	/**
+	 * sortType에 해당하는 조건으로 pagination
+	 * Newest: 최신순, created_at desc, product_id asc
+	 * PriceAsc: 가격 낮은 순, amount asc, product_id asc
+	 * PriceDesc: 가격 높은 순, amount desc, product_id asc
+	 * Popularity: 인기 순(주문 많은 순), order_count desc, product_id asc
+	 * TopRated: 평점 높은 순(review score), avg_score desc, product_id asc
+	 */
+	@Transactional(readOnly = true)
+	public ProductSearchResponse searchProduct(ProductSearchRequest request) {
+		List<Product> products = switch (request.sortType()) {
+			case Newest -> productRepository.findByNewest(toQueryKeyword(request.keyword()), request.lastCreatedAt(),
+				request.lastProductId(), request.pageSize());
+			case PriceAsc -> productRepository.findByPriceAsc(toQueryKeyword(request.keyword()), request.lastAmount(),
+				request.lastProductId(), request.pageSize());
+			case PriceDesc -> productRepository.findByPriceDesc(toQueryKeyword(request.keyword()), request.lastAmount(),
+				request.lastProductId(), request.pageSize());
+			case Popularity -> productRepository.findByPopularity(toQueryKeyword(
+				request.keyword()), request.lastOrderCount(), request.lastProductId(), request.pageSize());
+			case TopRated -> productRepository.findByTopRated(toQueryKeyword(request.keyword()), request.lastScore(),
+				request.lastProductId(), request.pageSize());
+		};
+		return ProductSearchResponse.of(products);
+	}
+
+	private String toQueryKeyword(String keyword) {
+		return "%" + keyword + "%";
 	}
 }
