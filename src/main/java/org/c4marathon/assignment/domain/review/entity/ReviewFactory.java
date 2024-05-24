5925e88f243bb3c3a3eaba2df4ffc30ee2352b25
kariskan <zkfzpf56@naver.com>
2024-05-24 18:35:05 +0900

    feat: ReviewFactory private 생성자 추가

--- src/main/java/org/c4marathon/assignment/domain/review/entity/ReviewFactory.java
+++ src/main/java/org/c4marathon/assignment/domain/review/entity/ReviewFactory.java
@@ -2,6 +2,10 @@ package org.c4marathon.assignment.domain.review.entity;
 
 import org.c4marathon.assignment.domain.review.dto.request.ReviewCreateRequest;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ReviewFactory {
 
 	public static Review buildReview(Long consumerId, ReviewCreateRequest request) {
