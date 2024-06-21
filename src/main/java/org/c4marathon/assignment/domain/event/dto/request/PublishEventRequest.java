77d63428fde2d0d74db985cb36b58994454aeaeb
kariskan <zkfzpf56@naver.com>
2024-06-22 00:53:04 +0900

    fix: @Future 추가

--- src/main/java/org/c4marathon/assignment/domain/event/dto/request/PublishEventRequest.java
+++ src/main/java/org/c4marathon/assignment/domain/event/dto/request/PublishEventRequest.java
@@ -2,6 +2,7 @@ package org.c4marathon.assignment.domain.event.dto.request;
 
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -11,6 +12,7 @@ public record PublishEventRequest(
 	@Size(max = 30)
 	String name,
 	@NotNull
+	@Future
 	LocalDateTime endDate
 ) {
 }
