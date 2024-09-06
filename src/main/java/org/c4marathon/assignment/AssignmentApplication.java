7de2f229784ef24ca8ee4d7fa7c9890d13a02c97
Lee Soo Kyung <dltnrud377@naver.com>
2024-09-06 17:48:24 +0900

    브랜치생성

--- src/main/java/org/c4marathon/assignment/AssignmentApplication.java
+++ src/main/java/org/c4marathon/assignment/AssignmentApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AssignmentApplication {
-
+//임시
 	public static void main(String[] args) {
 		SpringApplication.run(AssignmentApplication.class, args);
 	}
