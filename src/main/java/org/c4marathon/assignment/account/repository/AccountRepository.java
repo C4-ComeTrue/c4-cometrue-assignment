5de206c5acbc9b9ebe30ee9260b10a9e0fcab00a
jwj9127 <jwj9127@naver.com>
2024-09-11 04:12:29 +0900

    [feat]: 계좌 관련 Repository 추가

--- src/main/java/org/c4marathon/assignment/account/repository/AccountRepository.java
+++ src/main/java/org/c4marathon/assignment/account/repository/AccountRepository.java
@@ -1,9 +1,20 @@
 package org.c4marathon.assignment.account.repository;
 
 import org.c4marathon.assignment.account.domain.Account;
+import org.c4marathon.assignment.account.domain.AccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface AccountRepository extends JpaRepository<Long, Account> {
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional findByAccountNum(Long accountNum);
+
+    Optional findByAccountPw(int accountPw);
+
+    Optional<Account> findByUser_IdAndType(Long userId, AccountType type);
+
 }
