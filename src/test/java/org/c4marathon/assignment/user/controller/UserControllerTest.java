46fe3fd84d9d8f8dc968a52a9affef3e4b368d8b
jwj9127 <jwj9127@naver.com>
2024-09-10 22:24:07 +0900

    [test]: Test 코드 수정 중

---
+++ src/test/java/org/c4marathon/assignment/user/controller/UserControllerTest.java
@@ -0,0 +1,60 @@
+package org.c4marathon.assignment.user.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.c4marathon.assignment.user.dto.JoinDto;
+import org.c4marathon.assignment.user.repository.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private String toJson(Object object) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(object);
+    }
+
+    @BeforeEach
+    void start() {
+        userRepository.deleteAll();
+    }
+
+    @AfterEach
+    void end() {
+        userRepository.deleteAll();
+    }
+
+    @DisplayName("[회원가입 테스트]")
+    @Test
+    void joinTest() throws Exception {
+        // given
+        JoinDto joinDto = new JoinDto("abcd", "a1234", "홍길동");
+
+        // when, then
+        mockMvc.perform(post("/user/join")
+                        .content(toJson(joinDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
