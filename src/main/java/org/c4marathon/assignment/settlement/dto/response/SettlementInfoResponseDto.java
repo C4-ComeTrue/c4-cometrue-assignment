79bec7b555b01cf185d383c0eb161820e8673dc2
seungh1024 <seungh1024@naver.com>
2024-11-20 02:14:05 +0900

    refactor: 정산내역의 id가 timestamp와 date로 직렬화 되지 않도록 변경

--- src/main/java/org/c4marathon/assignment/settlement/dto/response/SettlementInfoResponseDto.java
+++ src/main/java/org/c4marathon/assignment/settlement/dto/response/SettlementInfoResponseDto.java
@@ -7,7 +7,11 @@ import org.bson.types.ObjectId;
 import org.c4marathon.assignment.settlement.document.MemberInfoDocument;
 import org.c4marathon.assignment.settlement.document.SettlementInfoDocument;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
 public record SettlementInfoResponseDto(
+	@JsonSerialize(using = ToStringSerializer.class)
 	ObjectId id,
 	long requestAccountPk,
 	String requestMemberName,
