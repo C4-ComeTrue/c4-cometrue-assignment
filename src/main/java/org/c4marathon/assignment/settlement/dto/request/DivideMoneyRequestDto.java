7ff4a9155d6a7f9bdd58154bb8741570bcea9303
seungh1024 <seungh1024@naver.com>
2024-11-19 18:38:58 +0900

    refactor: 최소 정산 금액을 1000원으로 변경

--- src/main/java/org/c4marathon/assignment/settlement/dto/request/DivideMoneyRequestDto.java
+++ src/main/java/org/c4marathon/assignment/settlement/dto/request/DivideMoneyRequestDto.java
@@ -6,7 +6,6 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 public record DivideMoneyRequestDto(
@@ -14,7 +13,7 @@ public record DivideMoneyRequestDto(
 	@Min(value = 1, message = "정산은 최소 1명 이상부터 가능합니다.")
 	int totalNumber,
 
-	@Positive(message = "정산 금액은 1원 이상부터 가능합니다.")
+	@Min(value = 1000, message = "정산 금액은 1000원 이상부터 가능합니다.")
 	long totalMoney,
 
 	@NotNull(message = "정산 방식을 입력해 주세요.")
