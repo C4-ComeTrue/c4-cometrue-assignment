d25d19b2a060929d8ed8e3cee88fcb171d035fcf
opixxx <opix0306@naver.com>
2025-01-21 19:58:32 +0900

    feat: 초기 셋팅

---
+++ src/main/java/org/c4marathon/global/entity/BaseEntity.java
@@ -0,0 +1,25 @@
+package org.c4marathon.global.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedBy
+    private LocalDateTime modifiedAt;
+}
---
+++ src/main/java/org/c4marathon/global/util/StringUtil.java
@@ -0,0 +1,12 @@
+package org.c4marathon.global.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.slf4j.helpers.MessageFormatter;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class StringUtil {
+    public static String format(String format, Object... objects) {
+        return MessageFormatter.arrayFormat(format, objects).getMessage();
+    }
+}
