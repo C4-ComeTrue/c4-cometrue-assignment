11f920b01b16f9c427c8e8511772142d3935a5b7
hellozo0 <ahju0011@gmail.com>
2025-01-27 23:22:48 +0900

    feat : 적금 계좌 Entity 생성

---
+++ src/main/java/org/c4marathon/assignment/domain/SavingAccount.java
@@ -0,0 +1,42 @@
+package org.c4marathon.assignment.domain;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "saving_account")
+@Getter
+@NoArgsConstructor
+public class SavingAccount {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "saving_account_id", nullable = false)
+	private Integer id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "main_account_id", nullable = false)
+	private MainAccount mainAccount;
+
+	@Column(name = "account_number", nullable = false, length = 15)
+	private String accountNumber;
+
+	@Column(name = "balance", nullable = false)
+	private int balance;
+
+	@Column(name = "rate", nullable = false)
+	private double rate;
+
+	@Column(name = "create_date", nullable = false)
+	private LocalDateTime createDate;
+}
