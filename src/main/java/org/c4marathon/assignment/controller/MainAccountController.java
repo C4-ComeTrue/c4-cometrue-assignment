760915162c537a38b80f1f9162f1886c57c626da
hellozo0 <ahju0011@gmail.com>
2025-01-28 05:59:54 +0900

    feat : 메인 계좌 충전
    - 로컬 캐시를 사용하려고 하였으나, 구현이 어려워 Redis를 통해 일일 한도 계산 로직 구현
    - 캐시에서 한도 계산 조건에 부합한지 먼저 체크
    - Balance를 추후에 수정
    - 만약 balance transaction이 실패했을 경우 캐시 값을 rollback할 수 있도록 구현
    - *redis lock을 구현한 부분은 추후 Scheduler commit때 설명 예정

--- src/main/java/org/c4marathon/assignment/controller/MainAccountController.java
+++ src/main/java/org/c4marathon/assignment/controller/MainAccountController.java
@@ -1,11 +1,24 @@
 package org.c4marathon.assignment.controller;
 
+import org.c4marathon.assignment.dto.request.ChargeMainAccountRequestDto;
+import org.c4marathon.assignment.service.MainAccountService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/account/main")
 @RequiredArgsConstructor
 public class MainAccountController {
+
+	private final MainAccountService mainAccountService;
+
+	@PostMapping("/charge")
+	public void chargeMainAccount(@RequestBody @Valid ChargeMainAccountRequestDto requestDto){
+		mainAccountService.chargeMainAccount(requestDto);
+	}
 }
