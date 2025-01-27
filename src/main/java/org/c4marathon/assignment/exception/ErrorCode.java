d8797fccb9de30c71b6149aace056daf8b8feaf2
nyh365 <12nyk34@naver.com>
2025-01-28 06:34:02 +0900

    feat: 예외처리 추가

--- src/main/java/org/c4marathon/assignment/exception/ErrorCode.java
+++ src/main/java/org/c4marathon/assignment/exception/ErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
     DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다."),
-    INVALID_EMAIL(HttpStatus.BAD_REQUEST, "존재하지 않는 이메일입니다.");
+    INVALID_EMAIL(HttpStatus.BAD_REQUEST, "존재하지 않는 이메일입니다."),
+    INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "Invalid parameter included");
 
     private final HttpStatus httpStatus;
     private final String message;
--- src/main/java/org/c4marathon/assignment/exception/GlobalExceptionHandler.java
+++ src/main/java/org/c4marathon/assignment/exception/GlobalExceptionHandler.java
@@ -1,27 +1,49 @@
 package org.c4marathon.assignment.exception;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+	@ExceptionHandler(CustomException.class)
+	public ResponseEntity<Object> handleCustomException(CustomException customException) {
+		ErrorCode errorCode = customException.getErrorCode();
+		return handleExceptionInternal(errorCode);
+	}
 
-    @ExceptionHandler(CustomException.class)
-    public ResponseEntity<Object> handleCustomException(CustomException customException) {
-        ErrorCode errorCode = customException.getErrorCode();
-        return handleExceptionInternal(errorCode);
-    }
+	private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode) {
+		return ResponseEntity.status(errorCode.getHttpStatus()).body(makeErrorResponse(errorCode));
+	}
 
-    private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode) {
-        return ResponseEntity.status(errorCode.getHttpStatus())
-                .body(makeErrorResponse(errorCode));
-    }
+	private ErrorResponse makeErrorResponse(ErrorCode errorCode) {
+		return ErrorResponse.builder().code(errorCode.name()).message(errorCode.getMessage()).build();
+	}
 
-    private ErrorResponse makeErrorResponse(ErrorCode errorCode) {
-        return ErrorResponse.builder()
-                .code(errorCode.name())
-                .message(errorCode.getMessage())
-                .build();
-    }
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+		MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+		List<ErrorResponse.ValidationError> validationErrors = ex.getBindingResult()
+			.getFieldErrors()
+			.stream()
+			.map(ErrorResponse.ValidationError::of)
+			.collect(Collectors.toList());
+
+		ErrorResponse errorResponse = ErrorResponse.builder()
+			.code(ErrorCode.INVALID_PARAMETER.name())
+			.message(ErrorCode.INVALID_PARAMETER.getMessage())
+			.errors(validationErrors)
+			.build();
+
+		return new ResponseEntity<>(errorResponse, ErrorCode.INVALID_PARAMETER.getHttpStatus());
+	}
 }
