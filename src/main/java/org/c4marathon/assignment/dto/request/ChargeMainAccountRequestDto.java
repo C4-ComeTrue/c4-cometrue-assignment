116105787a78d428873efbce45ecb63b2e4303e6
hellozo0 <ahju0011@gmail.com>
2025-01-28 06:06:33 +0900

    feat : 메인계좌 충전 Request

---
+++ src/main/java/org/c4marathon/assignment/dto/request/ChargeMainAccountRequestDto.java
@@ -0,0 +1,13 @@
+package org.c4marathon.assignment.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
+public record ChargeMainAccountRequestDto(
+	@NotNull(message = "메인계좌 id를 입력해주세요.")
+	long mainAccountId,
+	@NotNull(message = "충전 할 금액을 입력해주세요.")
+	@PositiveOrZero(message = "0원 이상을 입력해주세요.")
+	long money
+) {
+}
