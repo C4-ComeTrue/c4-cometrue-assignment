0bb7b1815513a540a096f5f78aebe9621c77026a
nyh365 <12nyk34@naver.com>
2025-01-28 06:22:31 +0900

    feat: 이메일로 회원 조회 기능 추가

--- src/main/java/org/c4marathon/assignment/repository/UserJpaRepository.java
+++ src/main/java/org/c4marathon/assignment/repository/UserJpaRepository.java
@@ -1,8 +1,11 @@
 package org.c4marathon.assignment.repository;
 
+import java.util.Optional;
+
 import org.c4marathon.assignment.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserJpaRepository extends JpaRepository<User, Long> {
 	boolean existsByEmail(String email);
+	Optional<User> findByEmail(String email);
 }
--- src/main/java/org/c4marathon/assignment/repository/UserRepository.java
+++ src/main/java/org/c4marathon/assignment/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package org.c4marathon.assignment.repository;
 
+import java.util.Optional;
+
 import lombok.RequiredArgsConstructor;
 
 import org.c4marathon.assignment.entity.User;
@@ -16,5 +18,9 @@ public class UserRepository {
 
 	public User save(User user) {
 		return userJpaRepository.save(user);
+	}
+
+	public Optional<User> findByEmail(String email) {
+		return userJpaRepository.findByEmail(email);
 	}
 }
