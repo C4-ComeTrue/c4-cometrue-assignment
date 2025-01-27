72d4751cf7a112c252b15710d149fb3be0aacef0
hellozo0 <ahju0011@gmail.com>
2025-01-28 06:07:34 +0900

    feat : 예외처리 및 세팅

--- src/main/java/org/c4marathon/assignment/AssignmentApplication.java
+++ src/main/java/org/c4marathon/assignment/AssignmentApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @EnableJpaAuditing
 @EnableCaching
 public class AssignmentApplication {
--- src/main/java/org/c4marathon/assignment/common/exception/enums/ErrorCode.java
+++ src/main/java/org/c4marathon/assignment/common/exception/enums/ErrorCode.java
@@ -8,7 +8,11 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 	//404
-	NOT_FOUND_MAIN_ACCOUNT(HttpStatus.NOT_FOUND, "메인 계좌가 존재하지 않습니다.");
+	NOT_FOUND_MAIN_ACCOUNT(HttpStatus.NOT_FOUND, "메인 계좌가 존재하지 않습니다."),
+	//409
+	DAILY_LIMIT_EXCEEDED(HttpStatus.CONFLICT, "일일 한도를 초과했습니다."),
+	REDIS_RESET_EXCEPTION(HttpStatus.CONFLICT, "일일 한도 초기화 중입니다."),
+	FAILED_BALANCE_UPDATE(HttpStatus.CONFLICT, "잔고 업데이트에 실패했습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
