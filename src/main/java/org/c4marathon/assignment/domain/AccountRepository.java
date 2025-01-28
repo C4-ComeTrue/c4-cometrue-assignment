6901a450c1e2a49f1f12d5e293da26bb5a56a709
ZZAMBAs <rksidksrksi@naver.com>
2025-01-28 13:21:10 +0900

    feat: 사용자, 계좌 관련 Repository 추가

---
+++ src/main/java/org/c4marathon/assignment/domain/AccountRepository.java
@@ -0,0 +1,19 @@
+package org.c4marathon.assignment.domain;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+	@Modifying
+	@Query("UPDATE Account a SET a.balance = a.balance + :money WHERE a.accountNumber = :accountNumber AND a.balance + :money >= 0")
+	int addBalance(@Param("money") long money, @Param("accountNumber") String accountNumber);
+
+	Optional<Account> findByAccountNumber(String accountNumber);
+
+	@Query("SELECT a FROM Account a WHERE a.userId = :userId AND a.isMain = true")
+	Optional<Account> findMainAccount(Long userId);
+}
