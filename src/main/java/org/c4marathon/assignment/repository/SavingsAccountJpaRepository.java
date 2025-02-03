01e95225fabda820612171080a88bc80f378efeb
nyh365 <12nyk34@naver.com>
2025-02-04 07:22:25 +0900

    refactor: 미사용 코드 제거

--- src/main/java/org/c4marathon/assignment/repository/SavingsAccountJpaRepository.java
+++ src/main/java/org/c4marathon/assignment/repository/SavingsAccountJpaRepository.java
@@ -6,7 +6,5 @@ import org.c4marathon.assignment.entity.SavingsAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SavingsAccountJpaRepository extends JpaRepository<SavingsAccount, Long> {
-	boolean existsByIdAndUserId(long savingsAccountId, long userId);
-
 	Optional<SavingsAccount> findByIdAndUserId(long savingsAccountId, long userId);
 }
