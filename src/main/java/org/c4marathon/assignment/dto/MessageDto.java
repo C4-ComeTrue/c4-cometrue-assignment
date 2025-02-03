965c0b2bf45f1ff3ad9e3eef973f515698527d98
nyh365 <12nyk34@naver.com>
2025-02-04 06:12:35 +0900

    refactor: 유효성 검증 코드 제거

--- src/main/java/org/c4marathon/assignment/dto/MessageDto.java
+++ src/main/java/org/c4marathon/assignment/dto/MessageDto.java
@@ -1,6 +1,5 @@
 package org.c4marathon.assignment.dto;
 
-import jakarta.validation.constraints.Positive;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,13 +10,11 @@ import lombok.ToString;
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MessageDto {
-	@Positive(message = "송금 트랜잭션 번호는 양수가 되어야 합니다.")
 	long transferTransactionId;
-	@Positive(message = "이체 계좌는 양수가 되어야 합니다.")
+
 	long senderMainAccount;
-	@Positive(message = "이체 받는 계좌는 양수가 되어야 합니다.")
+
 	long receiverMainAccount;
-	@Positive(message = "이체 금액은 양수가 되어야 합니다.")
 	long amount;
 
 	@Builder
--- src/main/java/org/c4marathon/assignment/dto/TransferTransactionEvent.java
+++ src/main/java/org/c4marathon/assignment/dto/TransferTransactionEvent.java
@@ -1,27 +1,15 @@
 package org.c4marathon.assignment.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class TransferTransactionEvent {
 	private static final int INIT_TRANSFER_TRANSACTION_ID = -1;
-	@Size(max = 100)
-	@NotBlank
 	String userName;
-
 	long transferTransactionId;
-
-	@Positive(message = "이체 계좌는 양수가 되어야 합니다.")
 	long senderMainAccount;
-
-	@Positive(message = "이체 받는 계좌는 양수가 되어야 합니다.")
 	long receiverMainAccount;
-
-	@Positive(message = "이체 금액은 양수가 되어야 합니다.")
 	long amount;
 
 	@Builder
