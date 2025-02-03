62237e2fc807d0a54f0b895ad12b590e7593afb3
ZZAMBAs <rksidksrksi@naver.com>
2025-02-03 18:03:17 +0900

    feat: 엔티티 변화에 따라 수정 및 커서 기반 페이징 쿼리 추가

--- src/main/java/org/c4marathon/assignment/domain/UserRepository.java
+++ src/main/java/org/c4marathon/assignment/domain/UserRepository.java
@@ -1,18 +1,24 @@
 package org.c4marathon.assignment.domain;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	@Modifying
-	@Query("UPDATE User u SET u.dayWithdrawLimit = 0 WHERE u.id = :id")
-	int initializeWithdrawLimit(Long id);
+	@Query("UPDATE User u SET u.chargeLimit = 0 WHERE u.id = :id")
+	int initChargeLimit(@Param("ids") Collection<Long> id);
 
 	@Modifying
-	@Query("UPDATE User u SET u.dayWithdraw = u.dayWithdraw + :money, u.lastWithdrawDate = :withdrawTime WHERE u.id = :id AND u.dayWithdraw + :money <= u.dayWithdrawLimit")
-	int withdraw(Long id, long money, LocalDateTime withdrawTime);
+	@Query("UPDATE User u SET u.accCharge = u.accCharge + :money WHERE u.id = :id AND u.accCharge + :money <= u.chargeLimit")
+	int charge(@Param("id") Long id, @Param("money") long money, @Param("chargeTime") LocalDateTime chargeTime);
+
+	@Query(value = "SELECT * FROM user u WHERE u.id > :cursor LIMIT :limit", nativeQuery = true)
+	List<User> findAllByCursor(@Param("cursor") long cursor, @Param("limit") int limit);
 }
