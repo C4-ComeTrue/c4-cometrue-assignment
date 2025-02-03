e6011e4fc86a0f8ed2e68774d3ad5cfd7973baba
ZZAMBAs <rksidksrksi@naver.com>
2025-02-03 21:49:22 +0900

    feat: 자정마다 충전 한도 초기화 하는 배치 연산 추가

---
+++ src/main/java/org/c4marathon/assignment/application/ScheduledService.java
@@ -0,0 +1,33 @@
+package org.c4marathon.assignment.application;
+
+import java.util.List;
+
+import org.c4marathon.assignment.domain.User;
+import org.c4marathon.assignment.domain.UserRepository;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ScheduledService {
+	private final UserRepository userRepository;
+
+	/**
+	 * 유저 인당 한도 초기화 벌크 연산
+	 */
+	@Scheduled(cron = "${user.charge-limit-initialization.execution-time}")
+	public void initChargeLimit() {
+		int limit = 1000;
+		List<User> users = userRepository.findAllByCursor(0, limit);
+		while (!users.isEmpty()) {
+			List<Long> userIds = users.stream().mapToLong(User::getId).boxed().toList();
+
+			userRepository.initChargeLimit(userIds);
+
+			users = userRepository.findAllByCursor(users.get(users.size() - 1).getId(), limit);
+		}
+
+	}
+}
