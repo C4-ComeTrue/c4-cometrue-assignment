df544c601efe11d2c86e1baa0d5700e5a488fdea
ZZAMBAs <rksidksrksi@naver.com>
2025-02-03 23:34:35 +0900

    feat: 트랜잭션 세분화

--- src/main/java/org/c4marathon/assignment/application/ScheduledService.java
+++ src/main/java/org/c4marathon/assignment/application/ScheduledService.java
@@ -3,7 +3,6 @@ package org.c4marathon.assignment.application;
 import java.util.List;
 
 import org.c4marathon.assignment.domain.User;
-import org.c4marathon.assignment.domain.UserRepository;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +11,7 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class ScheduledService {
-	private final UserRepository userRepository;
+	private final UserService userService;
 
 	/**
 	 * 유저 인당 한도 초기화 벌크 연산
@@ -20,13 +19,13 @@ public class ScheduledService {
 	@Scheduled(cron = "${user.charge-limit-initialization.execution-time}")
 	public void initChargeLimit() {
 		int limit = 1000;
-		List<User> users = userRepository.findAllByCursor(0, limit);
+		List<User> users = userService.findAllByCursor(0, limit);
 		while (!users.isEmpty()) {
 			List<Long> userIds = users.stream().mapToLong(User::getId).boxed().toList();
 
-			userRepository.initChargeLimit(userIds);
+			userService.initChargeLimit(userIds);
 
-			users = userRepository.findAllByCursor(users.get(users.size() - 1).getId(), limit);
+			users = userService.findAllByCursor(users.get(users.size() - 1).getId(), limit);
 		}
 
 	}
