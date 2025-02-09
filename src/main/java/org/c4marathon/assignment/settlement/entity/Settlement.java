5b64cb0df2131892bcb1a35cdb4405114a291b1f
opixxx <opix0306@naver.com>
2025-02-10 01:32:38 +0900

    feat: Settlement, SettlementDetail 엔티티 세팅

---
+++ src/main/java/org/c4marathon/assignment/settlement/entity/Settlement.java
@@ -0,0 +1,63 @@
+package org.c4marathon.assignment.settlement.entity;
+
+import static jakarta.persistence.FetchType.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.c4marathon.assignment.global.entity.BaseEntity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Settlement extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "settlement_id")
+	private Long id;
+
+	private Long requestAccountId; //정산 요청한 계좌 ID
+
+	private int totalAmount;
+
+	@Enumerated(EnumType.STRING)
+	private SettlementType type;
+
+	@OneToMany(mappedBy = "settlement", fetch = LAZY, cascade = CascadeType.ALL)
+	private List<SettlementDetail> settlementDetails = new ArrayList<>();
+
+	@Builder
+	private Settlement(Long requestAccountId, int totalAmount, SettlementType type) {
+		this.requestAccountId = requestAccountId;
+		this.totalAmount = totalAmount;
+		this.type = type;
+	}
+
+	public static Settlement create(Long requestAccountId, int totalAmount, SettlementType type) {
+		return Settlement.builder()
+			.requestAccountId(requestAccountId)
+			.totalAmount(totalAmount)
+			.type(type)
+			.build();
+	}
+
+	public void addSettlementDetail(SettlementDetail settlementDetail) {
+		this.settlementDetails.add(settlementDetail);
+		settlementDetail.setSettlement(this);
+
+	}
+}
---
+++ src/main/java/org/c4marathon/assignment/settlement/entity/SettlementType.java
@@ -0,0 +1,6 @@
+package org.c4marathon.assignment.settlement.entity;
+
+public enum SettlementType {
+	EQUAL,
+	RANDOM
+}
