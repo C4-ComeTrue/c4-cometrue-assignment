0bcf0f11332da5ea71d35d804eeb42cd6410a999
nyh365 <12nyk34@naver.com>
2025-02-11 01:34:53 +0900

    feat: 대상 유저 수 조회 기능 추가

--- src/main/java/org/c4marathon/assignment/repository/UserRepository.java
+++ src/main/java/org/c4marathon/assignment/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package org.c4marathon.assignment.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,7 @@ public class UserRepository {
 	public boolean isEmailExist(String email) {
 		return userJpaRepository.existsByEmail(email);
 	}
+
 	public User save(User user) {
 		return userJpaRepository.save(user);
 	}
@@ -22,7 +24,12 @@ public class UserRepository {
 	public Optional<User> findByEmail(String email) {
 		return userJpaRepository.findByEmail(email);
 	}
+
 	public Optional<User> findById(Long userId) {
 		return userJpaRepository.findById(userId);
+	}
+
+	public int countByIds(List<Long> userIds) {
+		return userJpaRepository.countByIds(userIds);
 	}
 }
