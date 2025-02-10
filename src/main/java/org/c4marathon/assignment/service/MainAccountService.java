f2ae4d7bd57ca6b89cb1ce26b48694165108c1ef
hellozo0 <ahju0011@gmail.com>
2025-02-11 05:35:24 +0900

    feat : 송금 transaction 의 보내는이의 잔고 처리
    - 보내는 사람의 계좌에서 돈을 차감하고 스케줄러를 통해 받는사람의 입금을 도와주는 로직으로 구현
    - @Scheduled + Redis 를 사용해서 순차적으로 처리하는 로직
    - [문제점] 순차처리로 인한 송금 딜레이 문제, 비효율적인 작업 처리
    - [개선방향] RedisStream이나 메시지큐 학습후 수정 예정

--- src/main/java/org/c4marathon/assignment/service/MainAccountService.java
+++ src/main/java/org/c4marathon/assignment/service/MainAccountService.java
@@ -9,11 +9,13 @@ import org.c4marathon.assignment.common.util.BankSystemUtil;
 import org.c4marathon.assignment.domain.MainAccount;
 import org.c4marathon.assignment.domain.User;
 import org.c4marathon.assignment.dto.request.ChargeMainAccountRequestDto;
+import org.c4marathon.assignment.dto.request.TransferRequestDto;
 import org.c4marathon.assignment.repository.MainAccountRepository;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
+
 
 import lombok.RequiredArgsConstructor;
 
@@ -32,21 +34,18 @@ public class MainAccountService {
 		mainAccountRepository.save(mainAccount);
 	}
 
-	public MainAccount getMainAccount(long mainAccountId){
-		return mainAccountRepository.findById(mainAccountId).orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND_MAIN_ACCOUNT));
-	}
-
-	public MainAccount getMainAccountWithXLock(long mainAccountId){
-		return mainAccountRepository.findByIdWithXLock(mainAccountId).orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND_MAIN_ACCOUNT));
-	}
-
-	public void withdrawMoney(MainAccount mainAccount, long money){
-		mainAccount.withdrawMoney(money);
-		mainAccountRepository.save(mainAccount);
+	/** [Step1] Redis에 Daily Limit 롤백
+	 * */
+	public void rollbackDailyChargeLimit(long mainAccountId, long money){
+		String key = "dailyLimit:" + mainAccountId;
+		Long currentLimit = redisTemplate.opsForValue().get(key);
+		if (currentLimit != null) {
+			redisTemplate.opsForValue().set(key, currentLimit + money);
+		}
 	}
 
 	/**
-	 * 메인 계좌 충전 Method
+	 * [Step1] 메인 계좌 충전 Method
 	 * [1] Redis를 통해 일일 충전 한도가 넘었는지 확인
 	 * [2] balance 업데이트 성공 했을 경우에 DB에도 저장
 	 * [3] balance 업데이트 실패했을때 rollback
@@ -73,7 +72,63 @@ public class MainAccountService {
 
 	}
 
-	/** [1]
+	/**
+	 * [Step2] 친구 계좌로 송금
+	 * 보내는 사람의 계좌에서 돈을 차감하고, 스케줄러를 통해서 돈을 추가해주기
+	 * 트랜잭션 범위를 최대한 분리
+	 * **/
+	@Transactional(isolation = Isolation.READ_COMMITTED)
+	public void transferToOtherAccount(TransferRequestDto requestDto) {
+
+		MainAccount sender = getMainAccountWithXLock(requestDto.senderAccountId());
+
+		long chargeAmount = checkBalanceAvailability(sender, requestDto.amount());
+		sender.withdrawMoney(requestDto.amount());
+		sender.updateChargeDate();
+		try {
+			mainAccountRepository.save(sender);
+			String key = "transfer:" + requestDto.senderAccountId() + ":" + requestDto.receiverAccountId() + ":" + chargeAmount;
+			redisTemplate.opsForValue().set(key, requestDto.amount());
+		} catch (Exception e){
+			rollbackDailyChargeLimit(requestDto.senderAccountId(),chargeAmount);
+		}
+	}
+
+	public MainAccount getMainAccount(long mainAccountId){
+		return mainAccountRepository.findById(mainAccountId).orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND_MAIN_ACCOUNT));
+	}
+
+	public MainAccount getMainAccountWithXLock(long mainAccountId){
+		return mainAccountRepository.findByIdWithXLock(mainAccountId).orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND_MAIN_ACCOUNT));
+	}
+
+	public void withdrawMoney(MainAccount mainAccount, long money){
+		mainAccount.withdrawMoney(money);
+		mainAccountRepository.save(mainAccount);
+	}
+
+	/**
+	 * [Step2] 송금시 잔액 부족 파악후 10,000단위로 충전
+	 * **/
+	private long checkBalanceAvailability(MainAccount mainAccount, long amount){
+		//송금 가능 여부 파악(잔고)
+		long chargeAmount = 0;
+		if (!mainAccount.checkBalanceAvailability(amount)) {
+
+			long needAmount = amount - mainAccount.getBalance();
+			if (needAmount > 0) { // 충전이 필요한 경우
+				chargeAmount = ((needAmount + 9999) / 10000) * 10000; //10,000원 단위로 올림
+
+				if (!validateDailyChargeLimit(mainAccount.getId(), chargeAmount)) { //redis에 일일 한도 확인
+					throw new IllegalStateException(ErrorCode.DAILY_LIMIT_EXCEEDED.getMessage());
+				}
+				mainAccount.chargeMoney(chargeAmount);
+			}
+		}
+		return chargeAmount;
+	}
+
+	/** [Step1] 일일 한도 체크
 	 * Redis Cache를 통해 일일 충전한도에 맞는지 여부 확인
 	 * */
 	private boolean validateDailyChargeLimit(long mainAccountId, long money){
@@ -99,7 +154,7 @@ public class MainAccountService {
 		return true;
 	}
 
-	/** [2]
+	/** [Step1] Main 계좌 잔고 수정
 	 * balance를 update하기 위해서 Lock 을 걸고 조회 수행
 	 * limit에도 -money 만큼 한도를 뺼수 있도록 조정
 	 * 트랜잭션 시간을 지금으로 설정
@@ -114,17 +169,6 @@ public class MainAccountService {
 			throw new BalanceUpdateException(ErrorCode.NOT_FOUND_MAIN_ACCOUNT);
 		}
 
-	}
-
-	/** [3]
-	 * balance를 update 실패 했을 때 rollback
-	 * */
-	private void rollbackDailyChargeLimit(long mainAccountId, long money){
-		String key = "dailyLimit:" + mainAccountId;
-		Long currentLimit = redisTemplate.opsForValue().get(key);
-		if (currentLimit != null) {
-			redisTemplate.opsForValue().set(key, currentLimit + money);
-		}
 	}
 
 }
