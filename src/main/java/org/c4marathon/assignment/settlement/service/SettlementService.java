70fdcca753e4df6584e44cb69bd3f39500628066
opixxx <opix0306@naver.com>
2025-02-11 00:30:22 +0900

    refactor: 랜덤 정산 Random -> ThreadLocalRandom으로 변경 - 랜덤 정산이 동시에 요청이 들어올 경우 여러 쓰레드가 동시에 경합 - 패배 - 재도전을 반복하며 성능상의 문제 발생 - ThreadLocalRandom을 사용해서 각 쓰레드마다 생성된 인스턴스가 각각 난수를 반환하도록 하여 경합 문제가 발생하지 않아 성능상 이점이 있다.

--- src/main/java/org/c4marathon/assignment/settlement/service/SettlementService.java
+++ src/main/java/org/c4marathon/assignment/settlement/service/SettlementService.java
@@ -3,6 +3,7 @@ package org.c4marathon.assignment.settlement.service;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -114,7 +115,7 @@ public class SettlementService {
 
 		// 총 인원 - 1명을 대상으로 10원 단위로 랜덤한 금액을 배정
 		List<Integer> amounts = IntStream.range(0, totalNumber - 1)
-			.mapToObj(i -> random.nextInt(totalAmountDivided - (totalNumber - i - 1)) + 1) // 최소 10원 이상 보장
+			.mapToObj(i -> ThreadLocalRandom.current().nextInt(totalAmountDivided - (totalNumber - i - 1)) + 1)
 			.sorted()
 			.toList();
 
