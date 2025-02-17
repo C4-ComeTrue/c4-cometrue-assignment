9b5ce5c86060abc498f3e1ec24547c32f3a47b99
ZZAMBAs <rksidksrksi@naver.com>
2025-02-17 23:34:51 +0900

    feat: 빌더 패턴 추가 및 생성 시간 자동 기록

--- src/main/java/org/c4marathon/assignment/domain/Transaction.java
+++ src/main/java/org/c4marathon/assignment/domain/Transaction.java
@@ -3,6 +3,7 @@ package org.c4marathon.assignment.domain;
 import java.time.LocalDateTime;
 
 import org.c4marathon.assignment.domain.type.TransactionState;
+import org.springframework.data.annotation.CreatedDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -12,13 +13,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "transaction")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Transaction {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,6 +39,7 @@ public class Transaction {
 	@Column(name = "balance", nullable = false)
 	private Long balance;
 
+	@CreatedDate
 	@Column(name = "created_at", nullable = false)
 	private LocalDateTime createdAt;
 
@@ -41,4 +47,13 @@ public class Transaction {
 	@Column(name = "state", nullable = false, length = 20)
 	private TransactionState state;
 
+	@Builder
+	public Transaction(String senderAccountNumber, String receiverAccountNumber, Long balance, LocalDateTime createdAt,
+		TransactionState state) {
+		this.senderAccountNumber = senderAccountNumber;
+		this.receiverAccountNumber = receiverAccountNumber;
+		this.balance = balance;
+		this.createdAt = createdAt;
+		this.state = state;
+	}
 }
