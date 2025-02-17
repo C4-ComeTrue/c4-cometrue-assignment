eaa4e8bbfce585580916ffe75cfd98022aae15e5
ZZAMBAs <rksidksrksi@naver.com>
2025-02-17 17:39:34 +0900

    feat: 거래 내역 엔티티 생성

---
+++ src/main/java/org/c4marathon/assignment/domain/Transaction.java
@@ -0,0 +1,39 @@
+package org.c4marathon.assignment.domain;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "transaction")
+public class Transaction {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
+	private Long id;
+
+	@Column(name = "sender_account_number", nullable = false, length = 50)
+	private String senderAccountNumber;
+
+	@Column(name = "receiver_account_number", nullable = false, length = 50)
+	private String receiverAccountNumber;
+
+	@Column(name = "balance", nullable = false)
+	private Long balance;
+
+	@Column(name = "created_at", nullable = false)
+	private LocalDateTime createdAt;
+
+	@Column(name = "state", nullable = false, length = 20)
+	private String state;
+
+}
