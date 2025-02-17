ca49cd3a96b6a96ee33ca67c2dbdd2c94855d711
opixxx <opix0306@naver.com>
2025-02-18 00:30:22 +0900

    test: depositByMember() 테스트 코드 작성

--- src/test/java/org/c4marathon/assignment/account/service/DepositServiceTest.java
+++ src/test/java/org/c4marathon/assignment/account/service/DepositServiceTest.java
@@ -12,10 +12,14 @@ import org.c4marathon.assignment.account.domain.repository.AccountRepository;
 import org.c4marathon.assignment.account.exception.NotFoundAccountException;
 import org.c4marathon.assignment.transaction.domain.Transaction;
 import org.c4marathon.assignment.transaction.domain.repository.TransactionRepository;
+import org.c4marathon.assignment.transaction.exception.InvalidTransactionStatusException;
+import org.c4marathon.assignment.transaction.exception.NotFoundTransactionException;
+import org.c4marathon.assignment.transaction.exception.UnauthorizedTransactionException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 class DepositServiceTest extends IntegrationTestSupport {
 
@@ -70,6 +74,59 @@ class DepositServiceTest extends IntegrationTestSupport {
 			.orElseThrow(NotFoundAccountException::new);
 
 		assertThat(updatedReceiverAccount.getMoney()).isEqualTo(21000L);
+	}
+
+	@Transactional
+	@DisplayName("송금을 받은 사용자가 직접 확인 후 금액을 수령한다.")
+	@Test
+	void depositByReceiver() throws Exception {
+	    // given
+		Account receiverAccount = createAccount(10000L);
+		Transaction transaction = Transaction.create(1L, receiverAccount.getId(), 2000L, PENDING_TRANSFER,
+			PENDING_DEPOSIT, LocalDateTime.now().minusHours(1));
+		transactionRepository.save(transaction);
+
+	    // when
+		depositService.depositByReceiver(receiverAccount.getId(), transaction.getId());
+
+	    // then
+		Account updatedReceiverAccount = accountRepository.findById(receiverAccount.getId())
+			.orElseThrow(NotFoundAccountException::new);
+		Transaction updatedTransaction = transactionRepository.findById(transaction.getId())
+			.orElseThrow(NotFoundTransactionException::new);
+
+		assertThat(updatedReceiverAccount.getMoney()).isEqualTo(12000L);
+		assertThat(updatedTransaction.getStatus()).isEqualTo(SUCCESS_DEPOSIT);
+	}
+
+	@DisplayName("송금 내역 상태가 PENDING_DEPOSIT이 아닌 송금 내역을 수령 시 예외가 발생한다.")
+	@Test
+	void depositByReceiverWithInvalidTransactionStatus() throws Exception {
+	    // given
+		Account receiverAccount = createAccount(10000L);
+		Account otherReceiverAccount = createAccount(10000L);
+		Transaction transaction = Transaction.create(1L, receiverAccount.getId(), 2000L, PENDING_TRANSFER,
+			SUCCESS_DEPOSIT, LocalDateTime.now().minusHours(1));
+		transactionRepository.save(transaction);
+
+	    // when // then
+		assertThatThrownBy(() -> depositService.depositByReceiver(receiverAccount.getId(), transaction.getId()))
+			.isInstanceOf(InvalidTransactionStatusException.class);
+	}
+
+	@DisplayName("송금 내역 상태가 PENDING_DEPOSIT이 아닌 송금 내역을 수령 시 예외가 발생한다.")
+	@Test
+	void depositByReceiverWithUnauthorizedTransaction() throws Exception {
+		// given
+		Account receiverAccount = createAccount(10000L);
+		Account otherReceiverAccount = createAccount(10000L);
+		Transaction transaction = Transaction.create(1L, receiverAccount.getId(), 2000L, PENDING_TRANSFER,
+			PENDING_DEPOSIT, LocalDateTime.now().minusHours(1));
+		transactionRepository.save(transaction);
+
+		// when // then
+		assertThatThrownBy(() -> depositService.depositByReceiver(otherReceiverAccount.getId(), transaction.getId()))
+			.isInstanceOf(UnauthorizedTransactionException.class);
 	}
 
 	private Account createAccount(long money) {
