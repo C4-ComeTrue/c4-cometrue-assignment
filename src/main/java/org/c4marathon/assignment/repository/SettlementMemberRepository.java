e927c366e25c3e7d16d6dc4309153b88ade9c0b8
hellozo0 <ahju0011@gmail.com>
2025-02-18 14:08:12 +0900

    feat : 정산 Service 로직
    - 정산을 위한 송금 요청 (PENDING)
    - 정산 요청 수락 (코드 미완)
    - 72시간 초과시 자동 취소 (커서 페이징으로 구현)
    - 24시간마다 남은 송금 리마인드 알림 (5분마다)

--- src/main/java/org/c4marathon/assignment/repository/SettlementMemberRepository.java
+++ src/main/java/org/c4marathon/assignment/repository/SettlementMemberRepository.java
@@ -2,7 +2,6 @@ package org.c4marathon.assignment.repository;
 
 import java.util.Optional;
 
-import org.c4marathon.assignment.domain.Settlement;
 import org.c4marathon.assignment.domain.SettlementMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
@@ -15,4 +14,6 @@ public interface SettlementMemberRepository extends JpaRepository<SettlementMemb
 	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	@Query("SELECT sm FROM SettlementMember sm WHERE sm.settlement.id = :settlementId and sm.accountId = :memberAccountId")
 	Optional<SettlementMember> findByAccountIdWithXLock(@Param("settlementId") Long settlementId, @Param("memberAccountId") Long memberAccountId);
+
+	Optional<SettlementMember> findBySettlement(long settlementId);
 }
--- src/main/java/org/c4marathon/assignment/service/SettlementService.java
+++ src/main/java/org/c4marathon/assignment/service/SettlementService.java
@@ -1,13 +1,16 @@
 package org.c4marathon.assignment.service;
 
 import java.security.SecureRandom;
-import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.c4marathon.assignment.common.QueryExecuteTemplate;
+import org.c4marathon.assignment.common.exception.NotFoundException;
+import org.c4marathon.assignment.common.exception.enums.ErrorCode;
+import org.c4marathon.assignment.common.string.StringEnum;
 import org.c4marathon.assignment.domain.MainAccount;
 import org.c4marathon.assignment.domain.Settlement;
 import org.c4marathon.assignment.domain.SettlementMember;
@@ -19,8 +22,11 @@ import org.c4marathon.assignment.domain.enums.TransactionType;
 import org.c4marathon.assignment.dto.request.ReceiveSettlementRequestDto;
 import org.c4marathon.assignment.dto.request.RemittanceRequestDto;
 import org.c4marathon.assignment.dto.request.SettlementRequestDto;
+import org.c4marathon.assignment.repository.SettlementMemberRepository;
 import org.c4marathon.assignment.repository.SettlementRepository;
 import org.c4marathon.assignment.repository.TransactionRepository;
+import org.c4marathon.assignment.repository.TransactionRepositoryCursor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -28,6 +34,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
+import io.netty.util.internal.StringUtil;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -38,11 +45,16 @@ import lombok.extern.slf4j.Slf4j;
 public class SettlementService {
 
 	private final SettlementRepository settlementRepository;
+	private final SettlementMemberRepository settlementMemberRepository;
 	private final TransactionRepository transactionRepository;
+	private final TransactionRepositoryCursor transactionRepositoryCursor;
 	private final UserService userService;
 	private final MainAccountService mainAccountService;
 	private final JavaMailSender javaMailSender;
+	private final RedisTemplate<String,Long> redisTemplate;
+
 	private static final int MAX_RETRY_COUNT = 3;
+	private static final int BATCH_SIZE = 1000;
 
 	/**
 	 * [Step3] 정산
@@ -56,6 +68,170 @@ public class SettlementService {
 		} else {
 			divideRandom(requestDto);
 		}
+	}
+
+
+	/**
+	 * [Step4] 돈을 보냈다는 알림 보내는 함수
+	 * "송금 요청이 도착했습니다. 72시간 내에 수령해주세요!"
+	 * */
+	private void sendNotification(long requestAccountId, String title) {
+		int attempt = 0;
+		boolean isSent = false;
+
+		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+		MainAccount mainAccount = mainAccountService.getMainAccount(requestAccountId);
+		User user = userService.getUser(mainAccount.getUser().getId());
+
+		while (attempt < MAX_RETRY_COUNT && !isSent) {
+			try {
+				MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+				mimeMessageHelper.setTo(user.getEmail());
+				mimeMessageHelper.setSubject(title);
+				javaMailSender.send(mimeMessage);
+				isSent = true;
+			} catch (Exception e) {
+				attempt++;
+				log.warn("메일 전송 실패 ({}회차): {}, 에러: {}", attempt, user.getEmail(), e.getMessage());
+
+				if (attempt < MAX_RETRY_COUNT) {
+					try {
+						Thread.sleep(2000);
+					} catch (InterruptedException ie) {
+						Thread.currentThread().interrupt();
+						log.error("메일 전송 재시도 대기 중 인터럽트 발생: {}", ie.getMessage());
+					}
+				} else {
+					log.error("메일 전송 최종 실패: {}", user.getEmail());
+				}
+			}
+		}
+	}
+
+	/**
+	 * [Step4] 정산을 위한 송금 요청하기(대기)
+	 *
+	 * */
+	@Transactional(isolation = Isolation.READ_COMMITTED)
+	public void requestRemittanceMoney(RemittanceRequestDto requestDto) {
+		SettlementMember settlementMember = settlementMemberRepository.findBySettlement(requestDto.settlementId()).orElseThrow(() -> new NotFoundException(
+			ErrorCode.NOT_FOUND_SETTLEMENT_MEMBER));
+		Transaction transaction = new Transaction(requestDto.requestAccountId(), requestDto.settlementMemberAccountId(),settlementMember,
+			requestDto.amount(), TransactionStatus.REQUESTED, requestDto.type());
+		transactionRepository.save(transaction);
+
+		if (requestDto.type() == TransactionType.REALTIME) {
+			// recieveMoney(transaction);
+			// ✅ 돈 받는 로직 추가 예정
+		} else {
+			transaction.pendingTransaction(); // 상태 변환
+			transactionRepository.save(transaction);
+			sendNotification(requestDto.requestAccountId(), StringEnum.PENDING_MAIL_TITLE.toString());
+			//✅ 일단 내 계좌에서 돈을 빼고, 돈이 부족한지 파악하고, 충전가능하다면 충전하는 로직 기존의것 디벨롭해서 추가 예정
+		}
+
+	}
+
+	/**
+	 * [Step4] 돈을 받는 함수
+	 * 전체가 다 정산되면 Settlement에 status Success로 변경
+	 * @requestDto
+	 * long transactionId
+	 * long settlementMemberId
+	 * */
+	@Transactional
+	public void receiveMoney(ReceiveSettlementRequestDto requestDto) {
+		//(1) TransactionId를 조회해서 해당 transaction SUCCESS 상태 변환
+
+		//(2) SettlementMemberId로 SettlementMent 조회해서 => 모든 Settlement들 조회
+		//(3) Settlement에 있는 내역들이 모두 정산되었으면 SUCCESS로 상태 바꾸기 ( 부분이면 PENDING으로변경)
+		//(3-1) -> 이러기 위해서는 SettlementMember에 정산 여부 상태값이 다시 추가 되어야 할것 같다.
+
+		// queuePendingTransaction(senderId, recieverId, amount);
+	}
+
+	/**
+	 * [Step4] (72시간 초과 시 자동 취소)
+	 * */
+	@Scheduled(fixedRate = 60000) // 1분마다 실행
+	@Transactional
+	public void cancelExpiredTransactions() {
+		LocalDateTime expirationTime = LocalDateTime.now().minusHours(72);
+
+		QueryExecuteTemplate.<Transaction>selectAndExecuteWithCursorAndPageLimit(
+			-1,
+			BATCH_SIZE,
+			lastTransaction -> transactionRepositoryCursor.findExpiredTransactions(
+				expirationTime,
+				lastTransaction == null ? null : lastTransaction.getPendingDate(),
+				lastTransaction == null ? null : lastTransaction.getId(),
+				BATCH_SIZE
+			),
+			this::processExpiredTransactions
+		);
+
+	}
+
+	/**
+	 * [Step4] 만료된 송금 처리 (환불 + 상태 변경)
+	 */
+	@Transactional
+	public void processExpiredTransactions(List<Transaction> transactions) {
+		List<Long> transactionIds = new ArrayList<>();
+
+		for (Transaction transaction : transactions) {
+			transaction.cancelTransaction();
+			transactionIds.add(transaction.getId());
+		}
+		transactionRepository.updateExpiredTransactions(transactionIds);
+
+		//환불 송금 요청
+		for (Transaction transaction : transactions) {
+			refundToSender(transaction);
+		}
+	}
+
+	/**
+	 * [Step4] 24시간 남은 송금 리마인드 알림
+	 * 5분마다 스케줄러를 통해 24시간 남은 송금 조회
+	 * */
+	@Scheduled(fixedRate = 300000)
+	public void sendRemindersForPendingTransactions() {
+		LocalDateTime endDate = LocalDateTime.now().minusHours(24);
+
+		QueryExecuteTemplate.<Transaction>selectAndExecuteWithCursorAndPageLimit(
+			-1,
+			BATCH_SIZE,
+			lastTransaction -> transactionRepositoryCursor.findRemindableTransactions(
+				endDate,
+				lastTransaction == null ? null : lastTransaction.getPendingDate(),
+				lastTransaction == null ? null : lastTransaction.getId(),
+				BATCH_SIZE
+			),
+			this::processMailMultiThread
+		);
+	}
+
+	/**
+	 * 1안 : 멀티스레드+ 동기적으로 메일 전송
+	 * (2안 : 메일 전송을 비동기로 처리하고 메일 전송 실패 건에 대해서만 롤백하는밥법)
+	 * */
+	@Transactional
+	public void processMailMultiThread(List<Transaction> transactions) {
+		transactions.parallelStream().forEach(transaction -> {
+			sendNotification(transaction.getReceiverAccountId(), StringEnum.ALERT_24_MAIL_TITLE.toString());
+		});
+
+		updateMailSentStatus(transactions);
+	}
+
+	@Transactional
+	public void updateMailSentStatus(List<Transaction> transactions) {
+		List<Long> transactionIds = transactions.stream()
+			.map(Transaction::getId)
+			.toList();
+
+		transactionRepository.updateMailSent(transactionIds);
 	}
 
 	/**
@@ -135,116 +311,26 @@ public class SettlementService {
 		settlementRepository.save(settlement);
 	}
 
-	/**
-	 * [Step4] 정산을 위한 송금 요청하기(대기)
-	 *
-	 * */
-	@Transactional(isolation = Isolation.READ_COMMITTED)
-	public void requestRemittanceMoney(RemittanceRequestDto requestDto) {
-		Transaction transaction = new Transaction(requestDto.requestAccountId(), requestDto.settlementMemberAccountId(),
-			requestDto.amount(), TransactionStatus.REQUESTED, requestDto.type());
-		transactionRepository.save(transaction);
-
-		if (requestDto.type() == TransactionType.REALTIME) {
-			recieveMoney(transaction); // ✅  돈바로 받는 로직이랑 같을텐데 일단 내 계좌에서 돈을 빼는 로직은 구현  + 돈 보내는 로직 다 구현
-		} else {
-			transaction.pendingTransaction(); // 상태 변환
-			transactionRepository.save(transaction);
-			sendNotification(requestDto.requestAccountId());
-			//✅ 돈바로 받는 로직이랑 같을텐데 일단 내 계좌에서 돈을 빼는 로직은 구현
-		}
-
-	}
-
-
-	/**
-	 * [Step4] 돈을 받는 함수
-	 * 전체가 다 정산되면 Settlement에 status Success로 변경
-	 * */
-	@Transactional
-	public void receiveMoney(ReceiveSettlementRequestDto requestDto) {
-		//requestDto.transactionId, requestDto.settlementMemberId
-		Transaction transaction = transactionRepository.findByIdWithXLock(transactionId)
-			.orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND_TRANSACTION));
-
-		if (transaction.getStatus() != TransactionStatus.PENDING) {
-			throw new IllegalStateException("이미 완료된 송금입니다.");
-		}
-
-		MainAccount receiver = mainAccountRepository.findByIdWithXLock(transaction.getReceiverId())
-			.orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND_ACCOUNT));
-
-		receiver.updateBalance(transaction.getAmount());
-		transaction.completeTransaction();
-
-		mainAccountRepository.save(receiver);
-		transactionRepository.save(transaction);
-	}
-
-	/**
-	 * [Step4] 돈을 보냈다는 알림 보내는 함수
-	 * "송금 요청이 도착했습니다. 72시간 내에 수령해주세요!"
-	 * */
-	private void sendNotification(long requestAccountId) {
-		final String MAIL_SUBJECT = "송금 요청이 도착했습니다. 72시간 내에 수령해주세요!";
-		int attempt = 0;
-		boolean isSent = false;
-
-		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-		MainAccount mainAccount = mainAccountService.getMainAccount(requestAccountId);
-		User user = userService.getUser(mainAccount.getUser().getId());
-
-		while (attempt < MAX_RETRY_COUNT && !isSent) {
-			try {
-				MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
-				mimeMessageHelper.setTo(user.getEmail());
-				mimeMessageHelper.setSubject(MAIL_SUBJECT);
-				javaMailSender.send(mimeMessage);
-				isSent = true;
-			} catch (Exception e) {
-				attempt++;
-				log.warn("메일 전송 실패 ({}회차): {}, 에러: {}", attempt, user.getEmail(), e.getMessage());
-
-				if (attempt < MAX_RETRY_COUNT) {
-					try {
-						Thread.sleep(2000);
-					} catch (InterruptedException ie) {
-						Thread.currentThread().interrupt();
-						log.error("메일 전송 재시도 대기 중 인터럽트 발생: {}", ie.getMessage());
-					}
-				} else {
-					log.error("메일 전송 최종 실패: {}", user.getEmail());
-				}
-			}
-		}
-	}
-
-	/**
-	 * [Step4] 24시간 남은 송금 리마인드 알림
-	 * */
-	@Scheduled(fixedRate = 60000) // 1분마다 실행 ==> 굳이 10분 마다 해도될듯?
-	public void sendRemindersForPendingTransactions() {
-		List<Transaction> pendingTransactions = transactionRepository.findRemindableTransactions(LocalDateTime.now().minusHours(48));
-
-		for (Transaction transaction : pendingTransactions) {
-			sendNotification(transaction.getReceiverId(), "송금을 받을 시간이 24시간 남았습니다!");
-		}
-	}
-
-	/**
-	 * [Step4] (72시간 초과 시 자동 취소)
-	 * */
-	@Scheduled(fixedRate = 60000) // 1분마다 실행
-	@Transactional
-	public void cancelExpiredTransactions() {
-		List<Transaction> expiredTransactions = transactionRepository.findExpiredTransactions(LocalDateTime.now().minusHours(72));
-
-		for (Transaction transaction : expiredTransactions) {
-			transaction.cancelTransaction();
-			transactionRepository.save(transaction);
-			sendNotification(transaction.getSenderId(), "72시간이 지나 송금이 자동 취소되었습니다.");
+	private void queuePendingTransaction(long senderId, long recieverId, long amount) {
+		try {
+			String key = "transfer:" + senderId + ":" + recieverId + ":" + amount;
+			redisTemplate.opsForValue().set(key, amount);
+		} catch (Exception e) {
+			log.error("송금 환불 실패 (트랜잭션 ID: {}): {}", senderId, e.getMessage());
+			throw e;
 		}
 	}
 
 
+	private void refundToSender(Transaction transaction) {
+		try {
+			String key = "transfer:" + transaction.getReceiverAccountId() + ":" + transaction.getSenderAccountId() + ":" + transaction.getAmount();
+			redisTemplate.opsForValue().set(key, transaction.getAmount());
+		} catch (Exception e) {
+			log.error("송금 환불 실패 (트랜잭션 ID: {}): {}", transaction.getId(), e.getMessage());
+			throw e;
+		}
+	}
 }
+
+
