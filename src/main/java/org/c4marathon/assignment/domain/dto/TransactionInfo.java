042de81f86be9c9a73eb2cb304ce4334ff629061
ZZAMBAs <rksidksrksi@naver.com>
2025-02-22 20:35:40 +0900

    refactor: 재사용성 높은 인터페이스로 수정

--- src/main/java/org/c4marathon/assignment/domain/dto/TransactionInfo.java
+++ src/main/java/org/c4marathon/assignment/domain/dto/TransactionInfo.java
@@ -2,8 +2,9 @@ package org.c4marathon.assignment.domain.dto;
 
 import java.time.LocalDateTime;
 
-public interface TransactionRemindInfo {
+public interface TransactionInfo {
 	long getId();
+	String getSenderAccountNumber();
 	String getReceiverAccountNumber();
 	LocalDateTime getDeadline();
 	long getBalance();
