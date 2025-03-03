a82592afb19ba26be9454cdcd67e021617b116bb
ZZAMBAs <rksidksrksi@naver.com>
2025-03-03 20:51:54 +0900

    refactor: 변경된 QueryTemplate과 Instant에 맞게 수정

--- src/main/java/org/c4marathon/assignment/application/MailScheduledService.java
+++ src/main/java/org/c4marathon/assignment/application/MailScheduledService.java
@@ -1,9 +1,9 @@
 package org.c4marathon.assignment.application;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.function.Consumer;
-import java.util.function.Supplier;
 
 import org.c4marathon.assignment.domain.Account;
 import org.c4marathon.assignment.domain.AccountRepository;
@@ -23,58 +23,42 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 
-import static org.c4marathon.assignment.global.CommonUtils.Pair;
-
 @Service
 @RequiredArgsConstructor
 public class MailScheduledService {
-	private static final int REMIND_HOURS = 24;
+	private static final int REMIND_LIMIT_HOURS = 24;
 	private static final int BATCH_SIZE = 1000;
+	private static final int REMIND_INTERVAL_MINUTES = 5;
 
 	private final JavaMailSender mailSender;
 	private final TransactionRepository transactionRepository;
 	private final AccountRepository accountRepository;
 	private final UserRepository userRepository;
 
-	private static Pair<Long, LocalDateTime> cursor = Pair.of(0L, LocalDateTime.MIN); // {id, lastDeadline}
-
 	/**
-	 * 현재 시간부터 24시간 이후 시간 내 PENDING 상태인 거래 내역을 확인하고 메일로 보냅니다. 스레드 풀을 이용합니다.
-	 * 배치를 통해 수행하며 cursor를 통해 마지막으로 메일을 보낸 거래를 추적합니다. 이를 통해 동일 거래에 대해 중복 메일이 전송되지 않습니다.
-	 * 현재 cursor는 변수로 관리 중이지만 사실 캐시에 저장하는 데이터로, redis에 저장할 수도 있을 것 같습니다.
-	 * 현재는 메일을 제대로 전송하지 못해도 그냥 넘어갑니다. 이는 메일이 그렇게 크게 중요하다고 생각이 들지 않기 때문입니다.
+	 * 실행 시간부터 5분 전 시간까지 PENDING 거래 내역들 중 자동 취소까지
+	 * 24시간 남은 거래 내역을 모두 찾고 메일 전송.
 	 */
-	@Scheduled(cron = "${transaction.remind-interval}")
+	@Scheduled(cron = "${transaction.scheduled.interval.remind}")
 	public void remindPendingTransaction() {
-		LocalDateTime start = LocalDateTime.now();
-		LocalDateTime end = start.plusHours(REMIND_HOURS);
-		cursor = Pair.of(cursor.getT(), cursor.getR().isBefore(start) ? start : cursor.getR());
+		Instant end = Instant.now().truncatedTo(ChronoUnit.MINUTES).plus(REMIND_LIMIT_HOURS, ChronoUnit.HOURS);
+		Instant initStart = end.minus(REMIND_INTERVAL_MINUTES, ChronoUnit.MINUTES);
 
 		ReminderThreadPoolExecutor threadPoolExecutor = new ReminderThreadPoolExecutor();
 		threadPoolExecutor.init();
 
-		QueryTemplate.selectAndExecuteWithCursor(getRemindInfos(end), sendReminderAll(threadPoolExecutor), BATCH_SIZE);
+		QueryTemplate.selectAndExecuteWithCursor(
+			transactionInfo ->
+				transactionRepository.findAllInfoBy(
+					transactionInfo == null ? 0 : transactionInfo.getId(),
+					transactionInfo == null ? initStart : transactionInfo.getDeadline(),
+					end,
+					TransactionState.PENDING.name(),
+					BATCH_SIZE),
+			sendReminderAll(threadPoolExecutor),
+			BATCH_SIZE);
 
 		threadPoolExecutor.shutdown();
-	}
-
-	/**
-	 * BATCH_SIZE만큼 메일을 보낼 PENDING 거래 내역을 가져옵니다.
-	 * @param end
-	 * @return
-	 */
-	private Supplier<List<TransactionInfo>> getRemindInfos(LocalDateTime end) {
-		return () -> {
-			List<TransactionInfo> remindInfos = transactionRepository.findAllInfoBy(
-				cursor.getT(), cursor.getR(), end, TransactionState.PENDING.name(), BATCH_SIZE);
-
-			if (!remindInfos.isEmpty()) {
-				TransactionInfo lastInfo = remindInfos.get(remindInfos.size() - 1);
-				cursor.update(lastInfo.getId(), lastInfo.getDeadline());
-			}
-
-			return remindInfos;
-		};
 	}
 
 	private Consumer<List<TransactionInfo>> sendReminderAll(ReminderThreadPoolExecutor threadPoolExecutor) {
@@ -91,7 +75,7 @@ public class MailScheduledService {
 			);
 	}
 
-	private void sendReminder(String receiverEmail, LocalDateTime deadline, long money) {
+	private void sendReminder(String receiverEmail, Instant deadline, long money) {
 		try {
 			MimeMessage message = getMessage(receiverEmail);
 			message.setSubject("찾아가지 않은 금액이 있어요");
