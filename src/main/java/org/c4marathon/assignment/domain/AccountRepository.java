0963aa19aabd2251de8e3b227eec2f5ab33ffadd
ZZAMBAs <rksidksrksi@naver.com>
2025-03-04 12:58:38 +0900

    feat: 가독성 높이고 계좌 번호 리스트를 받아 관련 계좌를 전부 가져오는 쿼리 추가

--- src/main/java/org/c4marathon/assignment/domain/AccountRepository.java
+++ src/main/java/org/c4marathon/assignment/domain/AccountRepository.java
@@ -1,5 +1,6 @@
 package org.c4marathon.assignment.domain;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,11 +10,26 @@ import org.springframework.data.repository.query.Param;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 	@Modifying
-	@Query("UPDATE Account a SET a.balance = a.balance + :money WHERE a.accountNumber = :accountNumber AND a.balance + :money >= 0")
+	@Query(value = """
+			UPDATE Account a
+			SET a.balance = a.balance + :money
+			WHERE a.accountNumber = :accountNumber AND a.balance + :money >= 0
+		""")
 	int updateBalance(@Param("accountNumber") String accountNumber, @Param("money") long money);
 
 	Optional<Account> findByAccountNumber(String accountNumber);
 
-	@Query("SELECT a FROM Account a WHERE a.userId = :userId AND a.isMain = true")
+	@Query("""
+			SELECT a
+			FROM Account a
+			WHERE a.userId = :userId AND a.isMain = true
+		""")
 	Optional<Account> findMainAccount(@Param("userId") Long userId);
+
+	@Query("""
+			SELECT a
+			FROM Account a
+			WHERE a.accountNumber IN :accountNumbers
+		""")
+	List<Account> findAllByAccountNumber(@Param("accountNumbers") List<String> accountNumbers);
 }
